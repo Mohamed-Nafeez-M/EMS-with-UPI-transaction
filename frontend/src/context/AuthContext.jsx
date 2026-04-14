0fc38e000ec6e49cf8f5a086e0b5ba46942dd8ca
Mohamed-Nafeez-M <2301109066@ptuniv.edu.in>
2026-04-14 16:06:54 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useState, useEffect } from 'react'
 import axios from 'axios'
-
+const API_URL = import.meta.env.VITE_API_URL;
+axios.defaults.baseURL = API_URL;
 const AuthContext = createContext(null)
 
 export const AuthProvider = ({ children }) => {
